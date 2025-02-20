e5db358dee7f0d4c1623985b4fc25510b422530c
Nikman800 <76964182+Nikman800@users.noreply.github.com>
2025-02-20 11:47:38 +0530

    Updated the app so that it works locally.

--- server/auth.ts
+++ server/auth.ts
@@ -30,7 +30,7 @@ async function comparePasswords(supplied: string, stored: string) {
 
 export function setupAuth(app: Express) {
   const sessionSettings: session.SessionOptions = {
-    secret: process.env.REPL_ID!,
+    secret: process.env.REPL_ID || 'local-dev-secret',
     resave: false,
     saveUninitialized: false,
     store: storage.sessionStore,
